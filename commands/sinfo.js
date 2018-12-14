734609e7bad540e71b096e726128df7358ccedf3
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-14 20:24:38 +0000

    Update sinfo.js

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -3,15 +3,15 @@ const guildz = message.guild
 
 module.exports.run = (bot, message, args) => {
   const embed = new discord.RichEmbed(
-    .setTitle(`Server Info for ${guildz.name}!`)
-    .addField(`Members`,`They are ${guildz.members}`)
-    .addField(`Region`,`The server region is ${guildz.region}`)
-    .addField(`Created At`,`This server was created on ${guildz.createdAt}`)
-    .addField(`Roles`,`The roles in this server are ${guildz.roles}`)
-    .addField(`Verified`,`This servers verification status is ${guildz.verified}`)
-    .addField(`AFK Channel`,`The AFK channel for this server is ${guild.afkChannel}`)
-    .setTimestamp()
-    .setColor("RANDOM")
+    setTitle(`Server Info for ${guildz.name}!`)
+    addField(`Members`,`They are ${guildz.members}`)
+    addField(`Region`,`The server region is ${guildz.region}`)
+    addField(`Created At`,`This server was created on ${guildz.createdAt}`)
+    addField(`Roles`,`The roles in this server are ${guildz.roles}`)
+    addField(`Verified`,`This servers verification status is ${guildz.verified}`)
+    addField(`AFK Channel`,`The AFK channel for this server is ${guild.afkChannel}`)
+    setTimestamp()
+    setColor("RANDOM")
     message.channel.send({embed: embed})
   }
 
