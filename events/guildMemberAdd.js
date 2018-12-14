22a5434a1bbf99632655c4adee5e73501fad48e6
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-14 20:11:45 +0000

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,12 +1,12 @@
 const discord = require('discord.js')
+const raid = require('./raid.json')
 
 module.exports = (bot, member) => {
   console.log(`${member.user.username} just joined ${member.guild.name}.`)
-  
+
   // Actual Functions
-  
   const channel = member.guild.channels.find(c => c.name == "welcome")
-  
+
   if (!channel) return;
   const em = new discord.RichEmbed()
   .setTitle(`%s Guild Welcomer`.replace("%s", member.guild.name))
@@ -14,5 +14,15 @@ module.exports = (bot, member) => {
   .setTimestamp()
   .setFooter(`${member.guild.name} Guild Welcomer`)
   .setColor("GREEN")
-  message.channel.send({embed: em})
-}
+  message.channel.send({embed: em});
+
+// AB
+if(!member.id = raid) return;
+const em1 = new discord.RichEmbed()
+  .setTitle(`**Guild Warning**`)
+  .setDescription(`${member.displayName || member.user.username} just joined ${member.guild.name} but has been noticed by me as a raider of many servers! Please take responsable action (since I cant).`)
+  .setTimestamp()
+  .addField(`Member id: ${member.id} `)
+  .setFooter(`${member.guild.name} Guild Warner`)
+  .setColor("RED")
+  member.guild.owner.send({embed: em1});
