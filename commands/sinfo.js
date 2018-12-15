64791d8eb5d1e8b4794cbcd826a3d2193cd034e1
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-15 16:03:32 +0000

    Add files via upload

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -2,16 +2,19 @@ const discord = require('discord.js')
 const guildz = message.guild
 
 module.exports.run = (bot, message, args) => {
-  const embed = new discord.RichEmbed(
-    setTitle(`Server Info for ${guildz.name}!`)
-    .addField(`Members`,`They are ${guildz.members}`)
-    .addField(`Region`,`The server region is ${guildz.region}`)
-    .addField(`Created At`,`This server was created on ${guildz.createdAt}`)
-    .addField(`Roles`,`The roles in this server are ${guildz.roles}`)
-    .addField(`Verified`,`This servers verification status is ${guildz.verified}`)
-    .addField(`AFK Channel`,`The AFK channel for this server is ${guild.afkChannel}`)
-    message.channel.send({embed: embed})
-  )}
+  const embed = new discord.RichEmbed()
+    .setAuthor(`BlitzBot`, bot.user.displayAvatarURL)
+      .setTitle(`Server Info for ${guildz.name}!`)
+        .addField(`Members`,`They are ${guildz.members}`)
+        .addField(`Region`,`The server region is ${guildz.region}`)
+        .addField(`Created At`,`This server was created on ${guildz.createdAt}`)
+        .addField(`Roles`,`The roles in this server are ${guildz.roles}`)
+        .addField(`Verified`,`This servers verification status is ${guildz.verified}`)
+        .addField(`AFK Channel`,`The AFK channel for this server is ${guild.afkChannel}`)
+        .setTimestamp()
+        .setColor("RANDOM")
+      message.channel.send({embed: embed})
+  }
 
   module.exports.help = {
     name: "sinfo",
--- commands/uinfo.js
+++ commands/uinfo.js
@@ -2,15 +2,15 @@ const discord = require('discord.js')
 const userz = message.author
 
 module.exports.run = (bot, message, args) => {
-  const embed = new discord.RichEmbed(
-  .setAuthor(`BlitzBot`, user.displayAvatarURL)
-  .setTitle(`User Info for ${userz.username}!`)
-  .addField("User ID:", `Your user ID is ${user.id}`)
-  .addField(`Time Of Join server:`,`You joined this server at ${userz.joinedAt}`)
-  .addField(`Time Of Join Discord:`,`You joined discord at ${userz.user.createdAt}`)
-  .addField('Roles:','${message.author.roles}')
-  .setTimestamp()
-  .setColor("RANDOM")
+  const embed = new discord.RichEmbed()
+    .setAuthor(`BlitzBot`, bot.user.displayAvatarURL)
+      .setTitle(`User Info for ${userz.username}!`)
+        .addField("User ID:", `Your user ID is ${user.id}`)
+        .addField(`Time Of Join server:`,`You joined this server at ${userz.joinedAt}`)
+        .addField(`Time Of Join Discord:`,`You joined discord at ${userz.user.createdAt}`)
+        .addField('Roles:','${message.author.roles}')
+      .setTimestamp()
+    .setColor("RANDOM")
   message.channel.send({embed: embed})
 }
 
