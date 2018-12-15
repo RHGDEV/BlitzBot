df684ee846918959132c376da01e3eb2acd04fad
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-15 16:13:31 +0000

    Add files via upload

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -1,16 +1,15 @@
 const discord = require('discord.js')
-const guildz = message.guild
 
 module.exports.run = (bot, message, args) => {
   const embed = new discord.RichEmbed()
     .setAuthor(`BlitzBot`, bot.user.displayAvatarURL)
-      .setTitle(`Server Info for ${guildz.name}!`)
-        .addField(`Members`,`They are ${guildz.members}`)
-        .addField(`Region`,`The server region is ${guildz.region}`)
-        .addField(`Created At`,`This server was created on ${guildz.createdAt}`)
-        .addField(`Roles`,`The roles in this server are ${guildz.roles}`)
-        .addField(`Verified`,`This servers verification status is ${guildz.verified}`)
-        .addField(`AFK Channel`,`The AFK channel for this server is ${guild.afkChannel}`)
+      .setTitle(`Server Info for ${message.guild.name}!`)
+        .addField(`Members`,`They are ${message.guild.members}`)
+        .addField(`Region`,`The server region is ${message.guild.region}`)
+        .addField(`Created At`,`This server was created on ${message.guild.createdAt}`)
+        .addField(`Roles`,`The roles in this server are ${message.guild.roles}`)
+        .addField(`Verified`,`This servers verification status is ${message.guild.verified}`)
+        .addField(`AFK Channel`,`The AFK channel for this server is ${message.guild.afkChannel}`)
         .setTimestamp()
         .setColor("RANDOM")
       message.channel.send({embed: embed})
--- commands/uinfo.js
+++ commands/uinfo.js
@@ -1,13 +1,12 @@
 const discord = require('discord.js')
-const userz = message.author
 
 module.exports.run = (bot, message, args) => {
   const embed = new discord.RichEmbed()
     .setAuthor(`BlitzBot`, bot.user.displayAvatarURL)
-      .setTitle(`User Info for ${userz.username}!`)
-        .addField("User ID:", `Your user ID is ${user.id}`)
-        .addField(`Time Of Join server:`,`You joined this server at ${userz.joinedAt}`)
-        .addField(`Time Of Join Discord:`,`You joined discord at ${userz.user.createdAt}`)
+      .setTitle(`User Info for ${message.author.username}!`)
+        .addField("User ID:", `Your user ID is ${message.author.id}`)
+        .addField(`Time Of Join server:`,`You joined this server at ${message.author.joinedAt}`)
+        .addField(`Time Of Join Discord:`,`You joined discord at ${message.author.user.createdAt}`)
         .addField('Roles:','${message.author.roles}')
       .setTimestamp()
     .setColor("RANDOM")
