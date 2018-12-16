138d16a683bc17c808c95b47fe00969f01fc3a23
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-16 17:14:10 +0000

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,4 +1,5 @@
 const discord = require('discord.js')
+const config = require('./config.json')
 
 module.exports.run = (bot, message, args) => {
   if (!config.owners.includes(message.author.id)) return message.channel.send("Nope!");
