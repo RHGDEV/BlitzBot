92056e766b27f09c2ae73d92956a29057b72eebf
BlitzProducts <44275189+BlitzProducts@users.noreply.github.com>
2018-12-16 10:26:56 +0000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,12 @@
 const discord = require('discord.js')
 const raid = require('./raid.json')
+const antiwelcome = requre('./antiwelcome.json')
 
 module.exports = (bot, member) => {
   console.log(`${member.user.username} just joined ${member.guild.name}.`)
 
   // Actual Functions
+  if (!antiwelcome.antiwelcome.includes(member.guild.id)) return;
   const channel = member.guild.channels.find(c => c.name == "welcome")
 
   if (!channel) return;
@@ -17,7 +19,7 @@ module.exports = (bot, member) => {
   message.channel.send({embed: em});
 
 // AB
-if(!member.id = raid) return;
+if (!raid.raid.includes(member.id)) return;
 const em1 = new discord.RichEmbed()
   .setTitle(`**Guild Warning**`)
   .setDescription(`${member.displayName || member.user.username} just joined ${member.guild.name} but has been noticed by me as a raider of many servers! Please take responsable action (since I cant).`)
@@ -25,4 +27,5 @@ const em1 = new discord.RichEmbed()
   .addField(`Member id: ${member.id} `)
   .setFooter(`${member.guild.name} Guild Warner`)
   .setColor("RED")
-  member.guild.owner.send({embed: em1});
+  member.guild.owner.send({embed: em1}) 
+} ;
